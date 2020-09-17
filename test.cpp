46a52cf2cd47429772ba30701ab5e7f3e49c9095
jiangsir <jiangzero@gmail.com>
2020-09-17 12:19:08 +0800

    foratter

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	int a, b;
-	cin >> a >> b;
-	int c = a;
-	a = b;
-	b = c;
-	return 0;
+int main() {
+  int a, b;
+  cin >> a >> b;
+  int c = a;
+  cout << c << endl;
+  a = b;
+  b = c;
+  return 0;
 }
