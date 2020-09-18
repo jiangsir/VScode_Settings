061822c1fae216d39ded95a71c4bcac283a844e4
jiangsir <jiangzero@gmail.com>
2020-09-18 09:00:57 +0800

    format

--- hello.cpp
+++ hello.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-  int n, a=0, b=1;
-  cin >> n;
+    int n, a = 0, b = 1;
+    cin >> n;
 
-  for (int i=0; i<n; i++){
-    b = a + b;
-    a = b - a;
-    cout << b << endl;
-  }
-  return 0;
+    for (int i = 0; i < n; i++) {
+        b = a + b;
+        a = b - a;
+        cout << b << endl;
+    }
+    return 0;
 }
--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ n = int(input())
 a = 0
 b = 1
 for i in range(n):
-	b = a + b
-	a = b - a
-	print(b)
+    b = a + b
+    a = b - a
+    print(b)
