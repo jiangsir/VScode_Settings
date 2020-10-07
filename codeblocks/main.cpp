043c6c8749160dc2dde4359bdf4d395f681a2702
jiangsir <jiangzero@gmail.com>
2020-10-07 15:07:40 +0800

    codeblocks設定

--- codeblocks/main.cpp
+++ codeblocks/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int arra[] = {0, 0, 0};
+
 int main() {
     int n, a = 0, b = 1;
     cin >> n;
-
+    arra[0] = n;
     for (int i = 0; i < n; i++) {
         b = a + b;
         a = b - a;
+        arra[i%2] = a;
+        arra[(i+1)%2] = b;
         cout << b << endl;
     }
     return 0;
