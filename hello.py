2af93969dd5328c02bd2ee4ad6a4edbb225e0f2c
jiangsir <jiangzero@gmail.com>
2020-10-21 17:50:50 +0800

    hello

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 n = int(input())
-<<<<<<< HEAD
 a = 0
 b = 1
 for i in range(n):
@@ -7,11 +6,3 @@ for i in range(n):
     a = b - a
     print(b)
 
-=======
-sum = 1
-i = 1
-for i in range(1, n+1):
-    sum = sum*i
-
-print(sum)
->>>>>>> ae4ad6fe6f84cd1f5dfa1b299e7c49b2e8b7e912
