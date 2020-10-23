881eb6d9dd957638d90007d74c4133ece69fc79a
jiangsir <jiangzero@gmail.com>
2020-10-23 11:33:52 +0800

    1023

--- hello.py
+++ hello.py
@@ -1,7 +1,23 @@
-n = int(input())
-sum = 1
-i = 1
-for i in range(1, n+1):
-    sum = sum*i
+def solv(a1, b1, a2, b2):
+	if a1>a2:
+		a1, a2 = a2, a1
+		b1, b2 = b2, b1
+	if a2 <= b1:
+		print('Yes')
+	else:
+		print('No')
 
-print(sum)
+n = int(input())
+for i in range(n):
+	a1, b1, a2, b2 = map(int, input().split())
+	solv(a1, b1, a2, b2)
+
+
+
+
+#import random
+# for i in range(1000):
+# 	a1 = random.randint(0,200)
+# 	b1 = random.randint(100,100001)
+# 	a2 = random.randint(10,100001)
+# 	b2 = random.randint(10,100001)
