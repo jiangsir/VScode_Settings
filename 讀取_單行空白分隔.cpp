43efa0ad5af035ca55b7ffaf01458dda75cfa01e
jiangsir <jiangzero@gmail.com>
2020-12-11 22:37:03 +0800

    1211

--- 讀取_單行空白分隔.cpp
+++ 讀取_單行空白分隔.cpp
@@ -3,18 +3,17 @@
 比如：給定一行，包含若干個整數
 1 3 5 7 9
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     vector<int> vi;
     int x;
-    while( cin >> x){
+    while (cin >> x) {
         vi.push_back(x);
     }
 
-
-    for (int x: vi){
+    for (int x : vi) {
         cout << "讀到 x=" << x << "\n";
     }
 
