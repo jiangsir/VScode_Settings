e219a1182e4defc82b53bd14382caf4352f7aaf8
jiangsir <jiangzero@gmail.com>
2021-05-26 17:40:15 +0800

    20210526

---
+++ p1.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "我愛寫程式" << endl;
+    cout << "我更愛真理" << endl;
+    return 0;
+}
---
+++ p3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int y;
+    while (cin >> y) {
+        cout << (y > 0 ? y + 1911 : 1912 + y) << endl;
+    }
+    return 0;
+}
