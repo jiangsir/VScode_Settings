1bd5607b954a3b01a55174bad42b06212b3bb7a4
jiangsir <jiangzero@gmail.com>
2021-11-24 15:45:07 +0800

    pi acos(-1)

--- 高二進階字串處理/4-1_f683. 圓周率.cpp
+++ 高二進階字串處理/4-1_f683. 圓周率.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main() {
-    string pi = to_string(acos(-1));
-    int x;
-    cin >> x;
-    cout << pi[x + 1] << "\n";
+    // string pi = to_string(acos(-1));
+    double pi = acos(-1);
+    printf("%.20f\n", pi);
+    long long ll = pi * 1000000000000000000;
+    cout << ll << endl;
+    string pis = to_string((long long int)(pi * 1.0e+18));
+    cout << pis << endl;
     return 0;
 }
