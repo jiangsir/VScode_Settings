dd393bd0ae7e715de156a34979c3f28504613655
jiangsir <jiangzero@gmail.com>
2022-03-16 11:49:00 +0800

    run in a.out

--- hello.py
+++ hello.py
@@ -1,17 +1,1 @@
-def solv(a1, b1, a2, b2):
-	if a1>a2:
-		a1, a2 = a2, a1
-		b1, b2 = b2, b1
-	if a2 <= b1:
-		print('Yes')
-	else:
-		print('No')
-
-n = int(input())
-a = 0
-b = 1
-for i in range(n):
-    b = a + b
-    a = b - a
-    print(b)
-
+print('hello')
--- p1.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    cout << "我愛寫程式" << endl;
-    cout << "我更愛真理" << endl;
-    return 0;
-}
