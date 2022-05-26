6b0b0c56bbac5c7a3a005fd9b0b0867f26ba5d8b
jiangsir <jiangzero@gmail.com>
2022-05-26 08:57:00 +0800

    a.out

--- p1.cpp
+++ p1.cpp
@@ -2,7 +2,31 @@
 using namespace std;
 
 int main() {
-    cout << "我愛寫程式" << endl;
-    cout << "我更愛真理" << endl;
+    int n;
+    cin >> n;
+    int a[n] = {0};
+    cin >> a[0];
+    for (int i = 1; i < n; i++) {
+        cin >> a[i];
+        a[i] += a[i - 1];
+    }
+
+    for (int x : a) {
+        cout << x << " ";
+    }
+    cout << endl;
     return 0;
 }
+
+/**
+5
+1 2 3 4 5
+
+1 3 6 10 15
+
+
+a: 1 2 3 4 5 6 7 8 9 10
+
+b: 1 3 6 10 15 21 .....
+
+*/
